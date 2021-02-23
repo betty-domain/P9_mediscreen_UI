93fead5a32f1dba44a24a2e15e79ad6c3354be18
betty Domain <betty.domain@free.fr>
2021-02-23 15:48:32 +0100

    add request logging filter

---
+++ src/main/java/com/mediscreen/ui/config/RequestLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.mediscreen.ui.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilter {
+
+    @Bean
+    /**
+     * Define logFilter attributes
+     */
+    public CommonsRequestLoggingFilter logFilter()
+    {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+
+        return filter;
+    }
+}
