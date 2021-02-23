267479a3099db046f4b80f3b257f2cd1f0a27f41
betty Domain <betty.domain@free.fr>
2021-02-23 16:00:34 +0100

    create PatientContrller and endpoint for patients list

--- src/main/java/com/mediscreen/ui/controller/PatientController.java
+++ src/main/java/com/mediscreen/ui/controller/PatientController.java
@@ -1,2 +1,25 @@
-package com.mediscreen.ui.controller;public class PatientController {
+package com.mediscreen.ui.controller;
+
+import com.mediscreen.ui.model.Patient;
+import com.mediscreen.ui.proxies.PatientsProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class PatientController {
+
+    @Autowired
+    private PatientsProxy patientsProxyService;
+
+    @RequestMapping("/patient/list")
+    public String list(Model model) {
+        List<Patient> patientList = patientsProxyService.getAllPatients();
+
+        model.addAttribute("patientsList",patientList);
+        return "patient/list";
+    }
 }
