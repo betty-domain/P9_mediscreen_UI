3d64e502503bc716ed514ef306c9b3f6d012c22b
betty Domain <betty.domain@free.fr>
2021-02-23 15:29:11 +0100

    Configure MediscreenUI Application to find FeignClients classes

--- src/main/java/com/mediscreen/ui/MediscreenUiApplication.java
+++ src/main/java/com/mediscreen/ui/MediscreenUiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients("com.mediscreen.ui")
 public class MediscreenUiApplication {
 
     public static void main(String[] args) {
