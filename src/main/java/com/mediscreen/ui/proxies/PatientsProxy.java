fb6b6dfeda306c0e427752d6876ef7f9718c4acf
betty Domain <betty.domain@free.fr>
2021-02-24 12:16:43 +0100

    add addClient method to feignCleint Patient

--- src/main/java/com/mediscreen/ui/proxies/PatientsProxy.java
+++ src/main/java/com/mediscreen/ui/proxies/PatientsProxy.java
@@ -2,8 +2,12 @@ package com.mediscreen.ui.proxies;
 
 import com.mediscreen.ui.model.Patient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @FeignClient(name="mediscreen-patient", url="http://localhost:9101")
@@ -11,4 +15,7 @@ public interface PatientsProxy {
 
     @GetMapping(value = "/patients")
     List<Patient> getAllPatients();
+
+    @PostMapping(value="/patient/add")
+    Patient addPatient(@RequestParam String family, @RequestParam  String given, @RequestParam @DateTimeFormat(pattern ="yyyy-MM-dd") LocalDate birthdate, @RequestParam String sex, @RequestParam String address, @RequestParam String phone);
 }
