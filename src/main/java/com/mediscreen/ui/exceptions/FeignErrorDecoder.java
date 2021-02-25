8178fe7543b1d6be6bd51ff5329ea0cdd49079f5
betty Domain <betty.domain@free.fr>
2021-02-25 17:14:09 +0100

    Add patientNotFound exception

--- src/main/java/com/mediscreen/ui/exceptions/FeignErrorDecoder.java
+++ src/main/java/com/mediscreen/ui/exceptions/FeignErrorDecoder.java
@@ -5,7 +5,6 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,21 +23,25 @@ public class FeignErrorDecoder implements ErrorDecoder {
 
         Optional<ExceptionMessage> originalException = getOriginalException(response);
 
-        if (response.status() == HttpStatus.BAD_REQUEST.value())
-        {
-            return new PatientBadRequestException(originalException.map(ExceptionMessage::getMessage).orElse("Patient Bad Request"));
+        switch (response.status()) {
+            case 400: {
+                return new PatientBadRequestException(originalException.map(ExceptionMessage::getMessage).orElse("Patient Bad Request"));
+            }
+            case 404: {
+                return new PatientNotFoundException(originalException.map(ExceptionMessage::getMessage).orElse("Patient not Found"));
+            }
+            default:
+                return defaultErrorDecoder.decode(str, response);
         }
-
-        return defaultErrorDecoder.decode(str,response);
     }
 
     /**
      * Get original Exception from FeignException
+     *
      * @param response response containing feignException
      * @return Exception message if exist, null otherwise
      */
-    private Optional<ExceptionMessage> getOriginalException(Response response)
-    {
+    private Optional<ExceptionMessage> getOriginalException(Response response) {
         Optional<ExceptionMessage> exceptionMessage = Optional.empty();
         try {
             String body = new BufferedReader(response.body().asReader(StandardCharsets.UTF_8))
@@ -46,7 +49,7 @@ public class FeignErrorDecoder implements ErrorDecoder {
                     .collect(Collectors.joining("\n"));
 
             ObjectMapper mapper = new ObjectMapper();
-            exceptionMessage = Optional.of(mapper.readValue(body,ExceptionMessage.class));
+            exceptionMessage = Optional.of(mapper.readValue(body, ExceptionMessage.class));
 
         } catch (IOException ioException) {
             logger.error("Error during FeignException Decoder : " + ioException.getMessage());
