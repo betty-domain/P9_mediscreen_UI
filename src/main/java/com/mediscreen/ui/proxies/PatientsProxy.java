e6eb3f35b343a7d5c34522032e66ff4c2a13dd4d
betty Domain <betty.domain@free.fr>
2021-02-25 17:14:26 +0100

    add update and get endpooint on feignClient

--- src/main/java/com/mediscreen/ui/proxies/PatientsProxy.java
+++ src/main/java/com/mediscreen/ui/proxies/PatientsProxy.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
@@ -16,6 +17,14 @@ public interface PatientsProxy {
     @GetMapping(value = "/patients")
     List<Patient> getAllPatients();
 
+    @GetMapping("/patient")
+    Patient getPatient(@RequestParam Integer id);
+
     @PostMapping(value="/patient/add")
     Patient addPatient(@RequestParam String family, @RequestParam  String given, @RequestParam @DateTimeFormat(pattern ="yyyy-MM-dd") LocalDate dob, @RequestParam String sex, @RequestParam String address, @RequestParam String phone);
+
+    @PutMapping(value="/patient/update")
+    Patient updatePatient(@RequestParam Integer id, @RequestParam String family, @RequestParam  String given, @RequestParam @DateTimeFormat(pattern ="yyyy-MM-dd") LocalDate dob, @RequestParam String sex, @RequestParam String address, @RequestParam String phone);
+
+
 }
