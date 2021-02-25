01d2d6633f9b9cf23d259c4f4cc27a0e177785dd
betty Domain <betty.domain@free.fr>
2021-02-25 17:15:35 +0100

    add update get form and update post request

--- src/main/java/com/mediscreen/ui/controller/PatientController.java
+++ src/main/java/com/mediscreen/ui/controller/PatientController.java
@@ -1,5 +1,6 @@
 package com.mediscreen.ui.controller;
 
+import com.mediscreen.ui.exceptions.PatientNotFoundException;
 import com.mediscreen.ui.model.Patient;
 import com.mediscreen.ui.proxies.PatientsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
@@ -31,7 +34,7 @@ public class PatientController {
         return "patient/add";
     }
 
-    @RequestMapping("/patient/validate")
+    @PostMapping("/patient/validate")
     public String validate(@Valid Patient patient, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "patient/add";
@@ -44,6 +47,37 @@ public class PatientController {
         } catch (Exception exception) {
             model.addAttribute("errorAddingPatient", exception.getMessage());
             return "patient/add";
+        }
+    }
+
+    @GetMapping("/patient/update/{id}")
+    public String updatePatientForm(@PathVariable("id") Integer id, Model model) {
+        try {
+            Patient patient = patientsProxyService.getPatient(id);
+            model.addAttribute("patient", patient);
+            return "patient/update";
+        }
+        catch (PatientNotFoundException patientNotFoundException)
+        {
+            model.addAttribute("errorPatientNotFoundMessage",patientNotFoundException.getMessage());
+            model.addAttribute("patient", new Patient());
+            return "patient/update";
+        }
+    }
+
+    @PostMapping("/patient/update/{id}")
+    public String updatePatient(@PathVariable("id") Integer id, @Valid Patient patient, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "patient/update";
+        }
+
+        try {
+            patientsProxyService.updatePatient(patient.getId(), patient.getLastname(), patient.getFirstname(), patient.getBirthDate(), patient.getSex(), patient.getAddress(), patient.getPhone());
+            model.addAttribute("patientsList", patientsProxyService.getAllPatients());
+            return "redirect:/patient/list";
+        } catch (Exception exception) {
+            model.addAttribute("errorUpdatingPatient", exception.getMessage());
+            return "patient/update";
         }
     }
 }
